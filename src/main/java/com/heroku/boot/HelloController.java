3d6bac6cb5d2cec21f9950ecb7279be2453b2d31
Ara <ara.manjoncb@gmail.com>
2020-05-20 12:21:17 +0200

    added endpoint

--- src/main/java/com/heroku/boot/HelloController.java
+++ src/main/java/com/heroku/boot/HelloController.java
@@ -1,7 +1,10 @@
 package com.heroku.boot;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +23,14 @@ public class HelloController{
     return String.format("Hello %s!", name);
   }
 
-  @GetMapping("/greetings")
-  Iterable<Greeting> greetings(@RequestParam String message){
+  @PostMapping("/greetings")
+  String addGreetings(@RequestParam String message){
     greetingRepository.save(new Greeting(message));
+    return message + "introduced correctly";
+  }
+
+  @GetMapping("/greetings")
+  Iterable<Greeting> greetings(){
     return greetingRepository.findAll();
   }
 }
